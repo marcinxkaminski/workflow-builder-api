1bab918d4015482e8c947a2ac719413f0e33440b
Marcin Kamiński <Marcin.Kaminski@dreamlab.pl>
2019-08-16 08:58:12 +0200

    🎨 Creating files structure

---
+++ ml/builder.py
@@ -0,0 +1,15 @@
+async def get_available_blocks(self) -> dict:
+    # TODO: get available blocks from /data folder
+    return
+
+
+async def merge_blocks_to_file(self):
+    # TODO: Should merge selected blocks, get them from /data folder and save in one file, e.g. as
+    # the name we could use combination on blocks' ids. It allows us to cache previous resolut and not
+    # repeat the whole process again.
+    return
+
+
+async def build_workflow(self, blocks: dict) -> str:
+    # TODO: Should return link to created file in tmp folder.
+    return
---
+++ ml/config.py
@@ -0,0 +1,12 @@
+TMP_PATH = '/tmp'
+
+CLEANER = {
+    'path': TMP_PATH,
+    'age': 1800,  # seconds
+    'interval': 300  # seconds
+}
+
+BUILDER = {
+    'path': '/data',
+    'dest_path': TMP_PATH
+}
