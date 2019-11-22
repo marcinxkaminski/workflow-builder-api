ef8c3cc00b32ef0ad43cf3cb3c2a263331d04538
Marcin Kaminski <marcin.kaminski@dreamlab.pl>
2019-11-22 18:11:21 +0100

    ✅ Tests

--- wb/tests/test_api.py
+++ wb/tests/test_api.py
@@ -1,12 +1,11 @@
-# from starlette.testclient import TestClient
-# from main import app
+from starlette.testclient import TestClient
+from main import app
 
 
-# client = TestClient(app)
+client = TestClient(app)
 
 
 def test_get_availble_workflow_elements():
-    # response = client.get("/api/workflow-builder/elements")
-    # assert response.status_code == 200
-    # assert response.json() is not None
-    assert True
+    response = client.get("/api/workflow-builder/elements")
+    assert response.status_code == 200
+    assert response.json() is not None
